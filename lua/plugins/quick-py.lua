9597c6adae1e0d45c0e0107b25a4b516eed5466c
AuroBreeze <1732373074@qq.com>
2025-06-01 11:16:53 +0800

    feat(plugins): 更新插件版本并调整配置
    - 更新 lazy-lock.json 中多个插件的提交哈希值
    - 更新 quick-py 插件版本并增加配置选项
    - 移除 venv-selector 插件配置

--- lua/plugins/quick-py.lua
+++ lua/plugins/quick-py.lua
@@ -5,4 +5,19 @@ return {
     },
     lazy =true,
     event = "VeryLazy",
+    branch = "dev",
+
+    config = {
+    venv_names = { ".venv", "venv" },
+    python_path = nil,
+    runserver_cmd = nil, -- 运行自定义python命令 ，例如django： python manage.py runserver
+    lsp_config = {
+        typeCheckingMode = "off"
+    }, -- 语言服务器配置
+    -- 新增键位配置
+    keymaps = {
+        run_python = { "<leader>rp", ":RunPython<CR>", { desc = "Run Python file" } },
+        set_lsp = { "<leader>rl", ":SetLsp<CR>", { desc = "Set LSP for Python" } },
+    }
+}
 }
