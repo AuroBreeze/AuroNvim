daf79b811f6e8ba1beb8cfbe6e0c16850a82098b
AuroBreeze <1732373074@qq.com>
2025-10-23 20:05:14 +0800

    feat(betterterm): 修改终端快捷键并添加 ArchLinux WSL 终端支持
    - 将终端选择快捷键从 `<leader>tt` 更改为 `<leader>ts`
    - 添加新的快捷键 `<leader>tw` 用于打开 ArchLinux WSL 终端
    - 新增功能通过延迟发送命令确保终端正确初始化

--- lua/plugins/betterterm.lua
+++ lua/plugins/betterterm.lua
@@ -20,11 +20,24 @@ return {
       desc = 'Open BetterTerm 1',
     },
     {
-      '<leader>tt',
+      '<leader>ts',
       function()
         require('betterTerm').select()
       end,
       desc = 'Select terminal',
+    },
+    {
+      '<leader>tw',
+      function()
+        local betterTerm = require('betterTerm')
+        -- 打开一个新的终端(例如索引为2的终端)
+        betterTerm.open(1)
+        -- 延迟发送命令以确保终端已完全初始化
+        vim.defer_fn(function()
+          betterTerm.send('wsl -d archlinux\r\n')
+        end, 300)
+      end,
+      desc = 'Open ArchLinux WSL Terminal',
     }
   },
   opts = {
